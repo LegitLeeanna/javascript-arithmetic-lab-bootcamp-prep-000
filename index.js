1d30e695d0ef4960fa7aec4908134520079a4b8d
Leeanna Siegmund <1legitworld@gmail.com>
2020-11-12 18:25:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,65 @@
+ var number = 10
+
+function add() {
+  number += 5 
+}
+
+function subtract() {
+  number -= 5
+}
+
+function divide() {
+  number /= 5
+}
+
+ function multiply() {
+   number *= 5
+ }
+
+var number = 5
+
+function add(a, b) {
+   number = a += b 
+   return number
+}
+
+function subtract(a, b) {
+  number = a -= b 
+  return number 
+}
+
+function divide(a, b) {
+  number = a /= b 
+  return number 
+}
+
+function multiply(a, b) {
+  number = a *= b 
+  return number 
+}
+
+function increment(number) {
+ number++
+ return number 
+}
+
+function decrement(n) {
+  n--
+  return n 
+}
+
+
+function makeInt(n) {
+  return parseInt(n, 10)
+  
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+  
+}
+
+
+
+
+
